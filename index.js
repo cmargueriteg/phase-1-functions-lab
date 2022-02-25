ffb66dd3362783b7ba86a640e6dc94fe58c72885
Cindy Germain <cindymarguerite@gmail.com>
2022-02-25 16:42:22 -0700

    commit tree aef2e69256074857162f31874c1b526296a8ee8a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if(distance > 42) {
+        return distance - 42
+    } else {
+        return 42 - distance
+    }
+}
+
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+}
+
+
+function distanceTravelledInFeet(start, end){
+    const result = (end - start) * 264
+    if(start > end){
+return result * -1
+    } else { 
+        return result
+    }
+
+}
+
+function calculatesFarePrice(start, end){
+    const distance = distanceTravelledInFeet(start, end)
+    if(distance <= 400){
+        return 0
+    } else if(distance > 400 && distance < 2000){
+        return (distance - 400) * .02
+    } else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else if(distance > 2500){
+        return "cannot travel that far"
+    }
+}
+
